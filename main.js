86d7cbd8d8b3cc21ea17fd94fb22d04e078fb0d5
Hannahbronwing <hannahsdoc.2015@gmail.com>
2022-09-12 18:48:39 -0400

    commit tree 6f7e22ad29ace16f40a49ce30c8d0e14e9dd82c5

--- main.js
+++ main.js
@@ -3,9 +3,21 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM HAS LOADED")
+})
+// Add the `.hidden` class to the error modal in the HTML so it does not appear when the page first loads
+const errorModal = (document.querySelector("#modal"))
+errorModal.classList.add("hidden")
 
+// call find likes
+findLikes()
 
-
+function findLikes(){
+  const likeArr = document.querySelectorAll(".like-glyph")
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => console.log("YOU FOUND ME! LIKE!")) }
+  )}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
